3806a074bd99ee4ea6fa1ced34b1e9f6dab92510
Albert L.G. <albert@albertlg.com>
2026-03-19 12:23:25 +0100

    chore: setup sanity deployment appId

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
@@ -11,5 +11,7 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    // 🚀 Añadimos el appId aquí:
+    appId: 'fh8l2pt4t9pecd5oamgv15q1',
   }
 })
